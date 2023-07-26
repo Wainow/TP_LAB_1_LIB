afcb7fad1073bf72fb2e4355aefe17f73cec5c6b
Alexey Cherepanov <62390803+Wainow@users.noreply.github.com>
2023-07-26 11:57:03 +0600

    new release??

--- mylibrary/src/main/java/com/wainow/mylibrary/domain/Interactor.kt
+++ mylibrary/src/main/java/com/wainow/mylibrary/domain/Interactor.kt
@@ -1,13 +1,16 @@
 package com.wainow.tp_lab_1.domain
 
+import android.annotation.SuppressLint
 import android.content.Context
-
-import androidx.mynewmedia4.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.FilteringMediaSource
+import androidx.media3.exoplayer.source.MediaSource
 
 interface Interactor {
     fun numFrequency(input: Collection<Int>): Any
     fun mostFrequent(input: Collection<Number>): Collection<Int>
     fun getExoPlayer(context: Context): ExoPlayer
+    fun getFilteringMediaSource() : FilteringMediaSource?
 }
 
 class NumberInteractor: Interactor{
@@ -25,7 +28,16 @@ class NumberInteractor: Interactor{
             .map { it.num }
     }
 
+    @SuppressLint("UnsafeOptInUsageError")
     override fun getExoPlayer(context: Context): ExoPlayer {
+        val mediaSource : MediaSource? = null;
+        val filteringMediaSource = FilteringMediaSource(mediaSource!!, 0)
         return ExoPlayer.Builder(context).build();
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun getFilteringMediaSource(): FilteringMediaSource? {
+        val mediaSource: MediaSource? = null;
+        return FilteringMediaSource(mediaSource!!, 0)
     }
 }
